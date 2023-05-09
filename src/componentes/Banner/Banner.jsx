6c856c2e126c3caca5c30054ad2078ac20d217e8
Arthur <arthurtt08@gmail.com>
2023-05-08 21:58:33 -0300

    melhorando desigin e código do banner

--- src/componentes/Banner/Banner.jsx
+++ src/componentes/Banner/Banner.jsx
@@ -3,6 +3,7 @@ import styles from "./Banner.module.scss"
 import { Container } from "@mui/material"
 import BotaoPrincipal from "componentes/BotaoPrincipal"
 import { Link } from "react-router-dom"
+import header from "./header.jpg"
 
 export default function Banner({
   saudacao,
@@ -14,16 +15,20 @@ export default function Banner({
   return (
     <>
       <div className={styles.banner}>
-        <Container>
-          <div className={styles.banner__conteudo}>
-            <h1 className={styles.banner__conteudo__titulo}>{saudacao}</h1>
-            <h2 className={styles.banner__conteudo__titulo}>{titulo}</h2>
-            <p className={styles.banner__conteudo__objetivo}>{descricao}</p>
-            <Link to={path}>
-              <BotaoPrincipal texto={tituloBotao} />
-            </Link>
+        <div className={styles.cabecalho}>
+          <div className={styles.cabecalho__conteudo}>
+            <Container>
+              <h1 className={styles.cabecalho__conteudo__titulo}>{saudacao}</h1>
+              <h2 className={styles.cabecalho__conteudo__titulo}>{titulo}</h2>
+              <p className={styles.cabecalho__conteudo__objetivo}>
+                {descricao}
+              </p>
+              <Link to={path}>
+                <BotaoPrincipal texto={tituloBotao} />
+              </Link>
+            </Container>
           </div>
-        </Container>
+        </div>
       </div>
     </>
   )
