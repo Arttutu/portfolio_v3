21672cff485185667bdea7fe02215367f9c0c945
Arttutu <arthurtt08@gmail.com>
2023-06-21 22:43:43 -0300

    update banner removendo imagem de teste

--- src/componentes/Banner/index.jsx
+++ src/componentes/Banner/index.jsx
@@ -73,7 +73,6 @@ export default function Banner({ saudacao, titulo, descricao, icone }) {
               </Link>
             </Alinhador>
           </StyleConteudo>
-          <img src={logo}></img>
         </Box>
       </BannerStyle>
     </>
