f0133d16ba29dfd976f2e96bc2a2c3a935663dd1
Arttutu <arthurtt08@gmail.com>
2023-07-13 22:03:46 -0300

    testaanto estilos no cartaoHabilidade da borda

--- src/componentes/CartaoHabilidades/index.jsx
+++ src/componentes/CartaoHabilidades/index.jsx
@@ -6,13 +6,13 @@ const CardHabilidade = styled.div`
   display: flex;
   gap: 1em;
   align-items: center;
-  border: 1px solid ${(props) => props.theme.colors.corUm};
+  border: 3px solid ${(props) => props.cor};
   border-radius: 50px;
   padding: 15px;
 `
-export default function CartaoHabilidade({ icone, titulo }) {
+export default function CartaoHabilidade({ icone, titulo, cor }) {
   return (
-    <CardHabilidade>
+    <CardHabilidade cor={cor}>
       <CartaoIcones habilidade icones={icone} />
       <Texto menu>{titulo}</Texto>
     </CardHabilidade>
