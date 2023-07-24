623dc2791f2aeb64ee2400e025728954d23d9f06
Arttutu <arthurtt08@gmail.com>
2023-07-24 18:27:17 -0300

    adicionando icone ao botao dowload cv

--- src/componentes/MinhaDescricao/index.jsx
+++ src/componentes/MinhaDescricao/index.jsx
@@ -1,15 +1,15 @@
 import React from "react"
 import styled from "styled-components"
 import { MinhaHistoria } from "./assets/textos/MinhaDescricao"
-import BotaoPrincipal from "componentes/BotaoPrincipal"
 import { Box, Texto, Titulo } from "componentes/UI"
 import { BoxImagem } from "componentes/Banner/style"
 import CV from "./assets/cv_arthur.pdf"
 import Imagem from "componentes/Imagem"
 import { BsFillEnvelopePaperFill } from "react-icons/bs"
+import { corCinza } from "componentes/UI/variaveis"
 const IconEnvelope = styled(BsFillEnvelopePaperFill)`
   font-size: 25px;
-  color: ${(props) => props.theme.colors.corUm};
+  color: ${(props) => props.theme.colors.corDois};
 `
 const SobreStyles = styled.div`
   display: flex;
@@ -23,14 +23,33 @@ const SobreStyles = styled.div`
 const StylesConteudo = styled.div`
   text-align: center;
   display: flex;
+  align-items: center;
   width: 100%;
   margin: 0 auto;
   flex-direction: column;
   gap: 2rem;
 `
 const BoxBotao = styled.div`
-  width: 100%;
+  cursor: pointer;
+  width: 300px;
+
+  transition: 0.3s;
+  padding: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 10px;
+  background-color: ${(props) => props.theme.colors.corUm};
   gap: 2rem;
+  &:hover {
+    background-color: ${corCinza};
+  }
+`
+const StyleLink = styled.a`
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  gap: 1rem;
 `
 
 export default function MinhaDescricao() {
@@ -44,10 +63,10 @@ export default function MinhaDescricao() {
           <Titulo>Sobre mim</Titulo>
           <Texto>{MinhaHistoria}</Texto>
           <BoxBotao>
-            <a href={CV} target="_blank">
-              <BotaoPrincipal texto="Dowload do CV"></BotaoPrincipal>
+            <StyleLink href={CV} target="_blank">
+              <Texto secundary>Dowloand CV</Texto>
               <IconEnvelope />
-            </a>
+            </StyleLink>
           </BoxBotao>
         </StylesConteudo>
       </SobreStyles>
