dcbd651a1b796e035af2635020c15a205abe1528
Arttutu <arthurtt08@gmail.com>
2023-10-21 20:27:11 -0300

    trocando foto logo

--- src/componentes/Imagem/index.jsx
+++ src/componentes/Imagem/index.jsx
@@ -1,9 +1,10 @@
 import React from "react"
-import Foto from "./img/logo-avatar.png"
+import Foto from "./img/logo-avatar2.jpeg"
 import styled from "styled-components"
 
 const ImagemPrimary = styled.img`
   max-width: 100%;
+  height: 500px;
   border-radius: ${({ secundary }) => (secundary ? "10%" : "5%")};
   background-color: ${(props) => props.theme.colors.corDois};
   @media (max-width: 768px) {
