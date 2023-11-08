182cafe04ce4bbacb01bac0f77cdf95df37567ff
Arttutu <arthurtt08@gmail.com>
2023-11-08 09:25:38 -0300

    arrumand o dimensões

--- src/componentes/Imagem/index.jsx
+++ src/componentes/Imagem/index.jsx
@@ -3,12 +3,13 @@ import Foto from "./img/logo-avatar.png"
 import styled from "styled-components"
 
 const ImagemPrimary = styled.img`
-  max-width: 100%;
-  height: 500px;
-  border-radius: ${({ secundary }) => (secundary ? "10%" : "5%")};
+  max-width: 500px;
+  height: 400px;
+  border-radius: ${({ secundary }) => (secundary ? "100%" : "50%")};
   background-color: ${(props) => props.theme.colors.corDois};
   @media (max-width: 768px) {
     max-width: 300px;
+    height: 300px;
   }
 `
 export default function Imagem() {
